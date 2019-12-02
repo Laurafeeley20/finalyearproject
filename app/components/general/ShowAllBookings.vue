8b6591517f334a4696516cbd720e298931946640
Ciaran Cregan <ciarancregan21@gmail.com>
2019-12-02 21:56:12 +0000

    added in the show bookings component so that all the users bookings will be displayed here

---
+++ app/components/general/ShowAllBookings.vue
@@ -0,0 +1,33 @@
+<template>
+    <Page class="page">
+        <ActionBar class="action-bar">
+            <Label :text="username" @tap="$refs.drawer.nativeView.showDrawer()" />
+        </ActionBar>
+        <StackLayout>
+            <CardView class="card" elevation="40" radius="10" ios:shadowRadius="3" v-for="(booking, index) in bookings" :key="index">
+                <StackLayout class="card-layout">
+                    <Label class="h2">{{ bookings.booking_type }}</Label>
+                    <Label class="body" textWrap="true">{{ booking.booking_type === 'Private' ? 'Private: 1-on-1 with Christopher Shannon' : state.firstUserBooking.booking_type }} - {{ booking.date }} @ {{ booking.time }}</Label>
+                </StackLayout>
+            </CardView> 
+        </StackLayout>
+    </Page>
+</template>
+
+<script>
+    export default {
+        name: 'ShowAllBookings',
+        computed: {
+            username(){
+                return this.$store.getters['username']
+            },
+            bookings(){
+                return this.$store.getters['bookings']
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+.action-bar{background:#25325c; color:white;}
+</style>
